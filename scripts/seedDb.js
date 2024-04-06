b95fab8c5fe2f33d3c5a14625afddcabee535f1a
Rahul Raman <rahul1raman@gmail.com>
2024-04-07 04:32:57 +0530

    husky test

--- scripts/seedDb.js
+++ scripts/seedDb.js
@@ -8,7 +8,7 @@ async function seed() {
   await Profile.sync({ force: true });
   await Contract.sync({ force: true });
   await Job.sync({ force: true });
-  //insert data
+  // insert seed data
   await Promise.all([
     Profile.create({
       id: 1,
