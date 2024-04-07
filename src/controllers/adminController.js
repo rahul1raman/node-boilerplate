57637776d317e93da288c91084a6cf22a9823730
Rahul Raman <rahul1raman@gmail.com>
2024-04-07 20:21:31 +0530

    GET /best-clients implementation

--- src/controllers/adminController.js
+++ src/controllers/adminController.js
@@ -21,7 +21,19 @@ async function findBestProfession(req, res, next) {
  * Returns the clients who paid the most
  * for jobs in the query time period
  */
-async function findBestClients(req, res, next) {}
+async function findBestClients(req, res, next) {
+  try {
+    const { Job, Profile, Contract } = req.app.get('models');
+    const { start, end } = req.query;
+    const limit = req.query.limit ? parseInt(req.query.limit) : 2;
+
+    const adminReader = new AdminReader(Job, Profile, Contract);
+    const result = await adminReader.getBestClients(start, end, limit);
+    res.json(result);
+  } catch (err) {
+    next(err);
+  }
+}
 
 module.exports = {
   findBestProfession,
--- src/readers/adminReader.js
+++ src/readers/adminReader.js
@@ -63,6 +63,64 @@ class AdminReader {
       limit: 1,
     });
   }
+
+  async getBestClients(start, end, limit = 2) {
+    if (!start || !end || !Date.parse(start) || !Date.parse(end) || Date.parse(start) > Date.parse(end)) {
+      throw new BadRequestError('Invalid Params: Start and end date required');
+    }
+
+    /*
+        Equivalent SQL query:
+
+        SELECT 
+            `Contract->Client`.id AS id,
+            `Contract->Client`.firstName || ' ' || `Contract->Client`.lastName AS fullName,
+             SUM(Job.price) AS paid
+        FROM 
+            Jobs AS Job
+            INNER JOIN Contracts AS Contract ON Job.ContractId = Contract.id
+            INNER JOIN Profiles AS Client ON Contract.ClientId = Client.id
+        WHERE 
+            Job.paid = TRUE
+            AND Job.paymentDate BETWEEN [start] AND [end]
+        GROUP BY 
+            Contract.ClientId
+        ORDER BY 
+            paid DESC
+        LIMIT [limit];
+
+    */
+
+    return this.Job.findAll({
+      attributes: [
+        [col('Contract.Client.id'), 'id'],
+        [literal("`Contract->Client`.`firstName` || ' ' ||  `Contract->Client`.`lastName`"), 'fullName'],
+        [fn('SUM', col('price')), 'paid'],
+      ],
+      include: [
+        {
+          model: this.Contract,
+          attributes: [],
+          include: [
+            {
+              model: this.Profile,
+              as: 'Client',
+              attributes: ['id', 'firstName', 'lastName'],
+            },
+          ],
+        },
+      ],
+      where: {
+        paid: true,
+        paymentDate: {
+          [Op.between]: [start, end],
+        },
+      },
+      group: ['Contract.Client.id'],
+      order: [[literal('paid'), 'DESC']],
+      limit,
+    });
+  }
 }
 
 module.exports = AdminReader;
